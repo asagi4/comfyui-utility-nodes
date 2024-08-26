72f5a05a8c6c396e60763b4c83ba17f299560fc1
asagi4 <130366179+asagi4@users.noreply.github.com>
2024-08-26 21:53:58 +0300

    Add some utilities for modifying conds

--- __init__.py
+++ __init__.py
@@ -2,10 +2,13 @@ from .utils import jinja_render as j
 from .utils import wildcards as w
 from .utils import memory as m
 from .utils import replace_model_weights as r
+from .utils import cond as c
 
 NODE_CLASS_MAPPINGS = {
     "MUJinjaRender": j.MUJinjaRender,
     "MUSimpleWildcard": w.MUSimpleWildcard,
     "MUForceCacheClear": m.MUForceCacheClear,
     "MUReplaceModelWeights": r.MUReplaceModelWeights,
+    "MUConditioningCutoff": c.MUConditioningCutoff,
+    "MURemoveControlNet": c.MURemoveControlNet,
 }
---
+++ utils/cond.py
@@ -0,0 +1,70 @@
+import logging
+
+log = logging.getLogger("comfyui-misc-utils")
+
+
+class MURemoveControlNet:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "conds": ("CONDITIONING",),
+            }
+        }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    CATEGORY = "misc-utils"
+    FUNCTION = "apply"
+
+    def apply(self, conds):
+        res = []
+        for c in conds:
+            c = [c[0].clone(), c[1].copy()]
+            if "control" in c[1]:
+                del c[1]["control"]
+            if "control_apply_to_uncond" in c[1]:
+                del c[1]["control_apply_to_uncond"]
+            res.append(c)
+        return (res,)
+
+
+class MUConditioningCutoff:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "conds": ("CONDITIONING",),
+                "start_cutoff": ("FLOAT", {"min": 0.00, "max": 1.00, "default": 0.0, "step": 0.01}),
+                "end_cutoff": ("FLOAT", {"min": 0.00, "max": 1.00, "default": 1.0, "step": 0.01}),
+            }
+        }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    CATEGORY = "misc-utils"
+    FUNCTION = "apply"
+
+    def apply(self, conds, start_cutoff, end_cutoff):
+        res = []
+        new_start = 0.0
+        for c in conds:
+            start = c[1].get("start_percent", 0.0)
+            end = c[1].get("end_percent", 1.0)
+            if end < start_cutoff:
+                log.debug("Chose to remove prompt '%s'", c[1].get("prompt", "N/A"))
+                continue
+            if start >= end_cutoff:
+                log.debug("Chose to remove prompt '%s'", c[1].get("prompt", "N/A"))
+                continue
+            if end >= end_cutoff:
+                end = 1.0
+            c = [c[0].clone(), c[1].copy()]
+            c[1]["start_percent"] = new_start
+            c[1]["end_percent"] = end
+            new_start = end
+            res.append(c)
+
+        if len(res) == 0:
+            log.warn("Cutoff would filter all conds, using the last one")
+            res = conds[-1]
+
+        return (res,)
